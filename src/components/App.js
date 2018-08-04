2256f8785ebe6dd8e0331d9b238a12b0c8dfac01
Selin Cam <selinkulacam@gmail.com>
2018-08-04 13:20:17 +0100

    Burger menu added.

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,44 @@ import React, { Component } from "react";
 import MapContainer from "./MapContainer";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sidebarStyle: {width: '0px'},
+      fullContentStyle: {marginLeft: '0px'}
+    }
+  }
+
+  handleViewSidebar = (shouldBeDisplayed) => {
+    if (shouldBeDisplayed) {
+      this.setState({
+        sidebarStyle: {width: '250px'},
+        fullContentStyle: {marginLeft: '250px'}
+    });
+    } else if (!shouldBeDisplayed) {
+      this.setState({
+        sidebarStyle: {width: '0px'},
+        fullContentStyle: {marginLeft: '0px'}
+      });
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <MapContainer />
-      </div>
+      <React.Fragment>
+        <div id="left-sidebar" className="sidenav" style={this.state.sidebarStyle}>
+          <a href="javascript:void(0)" className="closebtn" onClick={() => this.handleViewSidebar(false)}>&times;</a>
+          <a href="#">Contact</a>
+        </div>
+        
+        <div id="fullContent" style={this.state.fullContentStyle}>
+          <span className="burgerIcon" onClick={() => this.handleViewSidebar(true)}>&#9776;</span>
+          <div id="mapContent">
+            <MapContainer />
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
